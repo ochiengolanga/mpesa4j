fe5fb0c287dd29722b6bc9ba2eab1d50cec04c89
Ochieng Olanga <ochiengolanga@gmail.com>
2021-01-29 20:23:41 +0300

    Spotless format

--- libs/mpesa4j/src/internal-http/java/com/github/ochiengolanga/mpesa4j/HttpResponseCode.java
+++ libs/mpesa4j/src/internal-http/java/com/github/ochiengolanga/mpesa4j/HttpResponseCode.java
@@ -24,7 +24,8 @@ public interface HttpResponseCode {
   // 406	Not Acceptable ??? You requested a format that isn???t json
   // 429	Too Many Requests
   // 500	Internal Server Error ??? We had a problem with our server. Try again later.
-  // 503	Service Unavailable ??? We???re temporarily offline for maintenance. Please try again later.
+  // 503	Service Unavailable ??? We???re temporarily offline for maintenance. Please try again
+  // later.
 
   int OK = 200;
   int BAD_REQUEST = 400;
